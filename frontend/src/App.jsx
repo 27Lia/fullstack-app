a35a2a0a823edea8a3608fe9d684bc74cfa6277b
27Lia <tjs4114@gmail.com>
2023-07-16 02:57:52 +0900

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+import {Outlet, Route, Routes } from 'react-router-dom' 
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Navbar from './layout/Navbar'
+import Footer from './layout/Footer'
+
+function Layout() {
+  return (
+    <div className='flex flex-col justify-between h-screen'>
+
+     <Navbar />
+      <main className='w-10/12 max-w-4xl mx-auto mb-auto'>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+
+
+function App() {
+  return (
+    <Routes>
+    <Route path='/' element={<Layout />} >
+      <Route index element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        
+    </Route>
+  </Routes>
+)
+}
+
+export default App
---
+++ frontend/src/layout/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex h-20 text-lg justify-center items-center'>
+     All rights reserved. 
+    </div>
+  )
+}
+
+export default Footer
